d6608c5408986b3aa048791c88fd719a4120acbf
candace pinero <candcae.pinero@gmail.com>
2022-05-08 15:10:13 -0400

    w p: on click button is working

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-//API key
+//API key rendering images 
 const url = 'https://api.thecatapi.com/v1/images/search?x-api-key=f01e75a9-45fc-4138-9d7b-e7fc6ca4790'
 
 //Elements variable
-const $findFriend = $('#findfriend-btn')
+const $imageContainer = $('#imageContainer')
+const $findFriend = $('#friend-btn')
 console.log($findFriend)
-const $breed = $('#breed')
-console.log($breed)
+
+
 
 //function to get image 
 function getImage() {
    //function to call api for cat images 
-    $.ajax(url).then(function(data) {
-        $('main').append(`<img src='${data[0].url}'></img>`)
+    $.ajax(url).then(function(data) {   
+        //empties the the imageContainer div tag and renders new img 
+        $imageContainer.empty();
+        //creates img tag and pulls from the API array data
+        $imageContainer.append(`<img src='${data[0].url}'></img>`)
+       
+    
         console.log(data[0])
+       
     })
 }
 
+
 //calling getImage function
 getImage();
+
 
 
 //next step creat a onclick function to render random photos when the button is clicked
 
 
 //button click function
-
+$('#friend-btn').on('click', function(event){
+    
+    
+    getImage()
+    
+   
+})
 
