4077f89a524a72732a2a7af1d8eef0909efa7792
candace pinero <candcae.pinero@gmail.com>
2022-05-12 17:18:07 -0400

    formatted

--- js/script.js
+++ js/script.js
@@ -11,20 +11,20 @@ console.log($findFriend)
 
 //function to get image 
 function getImage() {
-   //function to call api for cat images 
-    $.ajax(url).then(function(data) {   
-        
+    //function to call api for cat images 
+    $.ajax(url).then(function (data) {
+
         //empties the the imageContainer div tag and renders new img 
         $imageContainer.empty()
-        
-        
+
+
         //creates img tag and pulls from the API array data
         $imageContainer.append(`<img id="catImg" src='${data[0].url}'></img>`)
-       
-        
-    
+
+
+
         console.log(data[0])
-       
+
     })
 }
 
@@ -39,28 +39,28 @@ getImage();
 
 
 //button click function
-$('#friend-btn').on('click', function(){
-    
-    $('#catImg').fadeOut('2000', function(){
-       
+$('#friend-btn').on('click', function () {
+
+    $('#catImg').fadeOut('2000', function () {
+
         getImage()
-        
+
     })
-    
-    
-   
+
+
+
 })
 
 
 
-$('#adopt-btn').on('click', function(){
+$('#adopt-btn').on('click', function () {
     window.open("https://www.petfinder.com/", "_blank")
 })
 
-$('#cafe-btn').on('click', function(){
+$('#cafe-btn').on('click', function () {
     window.open("https://www.naughtycatcafe.com/visit-us", "_blank")
 })
 
-$('#catcare').on('click', function(){
+$('#catcare').on('click', function () {
     window.open("https://www.aspca.org/pet-care/cat-care/general-cat-care", "_blank")
 })
